6f6f5e38a2841e92f224d7a8d4afaaf2c700a95d
Adrian Tokle Storset <55131770+ATS-Hackerman@users.noreply.github.com>
2021-03-18 15:34:08 +0100

    Installed pygame and set up main def

--- snake.py
+++ snake.py
@@ -12,45 +12,62 @@ class cube(object):
     def __init__(self, start, dirnx=1, dirny=0, color=(255,0,0)):
         pass
     
+
     def move(self, dirnx, dirny):
         pass
     
+
     def draw(self, surface, eyes=False):
         pass
+
 
 class snake(object):
     def __init__(self, color, pos):
         pass
-    
+
+
     def move(self):
         pass
-    
+
+
     def reset(self,pos):
         pass
-    
+
+
     def addCube(self):
         pass
-    
+
+
     def draw(self, surface):
         pass
-    
+
+
 def drawGrid(w, rows, surface):
     pass
+
 
 def redrawWindow(surface):
     pass
 
+
 def randomSnack(rows, items):
     pass
 
-def main():
-    pass
-    
-rows =
-w =
-h =
 
-cube.rows = rows
-cube.w = w
+def main():
+    width = 500
+    height = 500
+    rows = 20
+    win = pygame.displa.set_mode((width, height))
+    s = snake ((255,0,0), (10, 10))
+    flag = True
+
+    clock = pygame.time.Clock()
+
+    while flag:
+        pygame.time.delay(50)
+        clock.tick(10)
+        redrawWindow(win)
+
 
 main()
